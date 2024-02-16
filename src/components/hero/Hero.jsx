3f580dda6d5ead6331c37f18c02944b4e244180f
Vladyslav Rybak <rubakvlad7@gmail.com>
2024-02-16 08:53:43 +0200

    feat: add a hero section

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,85 @@
+import { motion } from "framer-motion";
+
+import "./hero.scss";
+
+const textVariants = {
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+  scrollButton: {
+    opacity: 0,
+    y: 10,
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+    },
+  },
+};
+
+const sliderVariants = {
+  initial: {
+    x: 0,
+  },
+  animate: {
+    x: "-220%",
+    transition: {
+      repeat: Infinity,
+      repeatType: "mirror",
+      duration: 20,
+    },
+  },
+};
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="wrapper">
+        <motion.div
+          className="textContainer"
+          variants={textVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.h2 variants={textVariants}>VLADYSLAV RYBAK</motion.h2>
+          <motion.h1 variants={textVariants}>
+            Web Developer and UI designer
+          </motion.h1>
+          <motion.div className="buttons" variants={textVariants}>
+            <motion.button variants={textVariants}>
+              See The Latest Works
+            </motion.button>
+            <motion.button variants={textVariants}>Contact Me</motion.button>
+          </motion.div>
+          <motion.img
+            src="/scroll.png"
+            alt="Scroll image"
+            variants={textVariants}
+            animate="scrollButton"
+          />
+        </motion.div>
+      </div>
+      <motion.div
+        className="slidingTextContainer"
+        variants={sliderVariants}
+        initial="initial"
+        animate="animate"
+      >
+        Web Developer Creator Influencer
+      </motion.div>
+      <div className="imageContainer">
+        <img src="/hero.png" alt="Hero image" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
